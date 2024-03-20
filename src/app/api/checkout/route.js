4721ee1e202c390ad1122139d0d521a7a194de12
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-03-20 18:00:08 -0300

    app done

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,17 +1,66 @@
 import paypal from "@paypal/checkout-server-sdk";
 import { NextResponse } from "next/server";
 
-const clientId =
-  "AZfvpI2k6r8BGlAelt7pqbMbftPtk3F2oc8oWeDRIkd8_6QFwXp2m21BAqSNSPWuT3C6i63_0Ed9PbO0";
-const clientSecret =
-  "EFsSFlu0reLHxOkAReokvfUO4cWUzo-ngPCgkUZFGHWvRz2tam_D7OAhcc-TjSmyF3L14BthuuhYnyuz";
+const clientId = process.env.PAYPAL_CLIENT_ID;
+const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+("EFsSFlu0reLHxOkAReokvfUO4cWUzo-ngPCgkUZFGHWvRz2tam_D7OAhcc-TjSmyF3L14BthuuhYnyuz");
 
 const enviroment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 
-const client = new paypal.core.PayPalEnvironment(enviroment);
+const client = new paypal.core.PayPalHttpClient(enviroment);
 
-export function POST() {
-  return NextResponse.json({
-    message: "Procesando pago",
-  });
+export async function POST() {
+  try {
+    const request = new paypal.orders.OrdersCreateRequest();
+
+    request.requestBody({
+      intent: "CAPTURE",
+
+      // 'purchase_units' Son los productos
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: "100.00",
+            breakdown: {
+              item_total: {
+                currency_code: "USD",
+                value: "100.00",
+              },
+            },
+          },
+          items: [
+            {
+              name: "Book of React Js",
+              description: "The best book to learn React",
+              quantity: "1",
+              unit_amount: {
+                currency_code: "USD",
+                value: "40.00",
+              },
+            },
+            {
+              name: "How to learn Java",
+              description: "The best book to learn Java",
+              quantity: "1",
+              unit_amount: {
+                currency_code: "USD",
+                value: "60.00",
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    const response = await client.execute(request);
+    console.log(response);
+
+    return NextResponse.json({
+      id: response.result.id,
+    });
+  } catch (error) {
+    console.error("Error creating order:", error);
+    return NextResponse.error(error);
+  }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,7 @@ function page() {
     <div className="w-full h-screen flex justify-center items-center bg-zinc-900">
       <PayPalScriptProvider
         options={{
-          clientId:
-            "AZfvpI2k6r8BGlAelt7pqbMbftPtk3F2oc8oWeDRIkd8_6QFwXp2m21BAqSNSPWuT3C6i63_0Ed9PbO0",
+          clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
         }}
       >
         <PayPalButtons
@@ -20,8 +19,17 @@ function page() {
             const res = await fetch("/api/checkout", {
               method: "POST",
             });
-            const data = await res.json();
+            const order = await res.json();
+            console.log(order);
+            return order.id;
+          }}
+          onApprove={(data, actions) => {
             console.log(data);
+
+            actions.order.capture();
+          }}
+          onCancel={(data) => {
+            console.log("Cancelled order:", data);
           }}
         />
       </PayPalScriptProvider>
